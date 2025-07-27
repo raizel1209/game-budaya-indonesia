28ac4afcedc0768f6697c4f000cd2bf023314bb7
raizel1209 <izzatffr12@gmail.com>
2025-07-27 17:00:07 +0700

    menambahkan TrustProxies

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,24 @@
+// app/Http/Middleware/TrustProxies.php
+
+// ...
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*'; // Change this from null or an empty array
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
+}
